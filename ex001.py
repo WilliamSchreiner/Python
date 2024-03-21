1efa40b31d58ce49ee1991742cb57c5a525447b9
William Gabriel Schreiner <106719508+WilliamSchreiner@users.noreply.github.com>
2024-03-21 13:36:07 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,4 @@
+print("Ola, mundo!")
+
+print("Esta gama de exercicios visa aprender e praticar python")
+print("todos os exerciciso estao presentes no canal do youtube: Curso em Vídeo")
---
+++ ex003.py
@@ -0,0 +1,6 @@
+x1 = float(input("Digite o primeiro numero: "));
+x2 = float(input("Digite o psecundo numero: "));
+
+soma = x2 + x1;
+
+print("A soma dos numeros {} e {} foi de {}".format(x1, x2, soma))
